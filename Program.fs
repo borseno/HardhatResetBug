3e6e10992eedaf6f905ccef0e6540144a0568c85
Mark Virchenko <mark.virchenko@redduck.io>
2021-05-19 10:26:46 +0300

    Added working HardhatResetInput according to https://github.com/Nethereum/Nethereum/issues/680

--- Program.fs
+++ Program.fs
@@ -11,12 +11,6 @@ let alchemyKey = "<your_key>"
 
 let hardhatURI = "http://localhost:8545"
 
-type Forking() =
-    member val jsonRpcUrl  = sprintf "https://eth-mainnet.alchemyapi.io/v2/%s" alchemyKey
-    member val blockNumber = 12330245
-type HardhatReset() =
-    member val forking = Forking()
-
 let inline runNow task =
     task
     |> Async.AwaitTask
@@ -28,6 +22,21 @@ let inline runNowWithoutResult (task:Task) =
 let printBlockNumber (web3:Web3) a = 
     web3.Eth.Blocks.GetBlockNumber.SendRequestAsync() |> runNow |> a |> printfn "%s"
 
+type HardhatForkInput() =
+    [<JsonProperty(PropertyName = "jsonRpcUrl")>]
+    member val JsonRpcUrl = "" with get, set
+    [<JsonProperty(PropertyName = "blockNumber")>]
+    member val BlockNumber = 0UL with get, set
+
+type HardhatResetInput() =
+    [<JsonProperty(PropertyName = "forking")>]
+    member val Forking = HardhatForkInput() with get, set
+
+type HardhatReset(client) = 
+    inherit RpcRequestResponseHandler<bool>(client, "hardhat_reset")
+
+    member __.SendRequestAsync (input:HardhatResetInput) (id:obj) = base.SendRequestAsync(id, input);
+
 [<EntryPoint>]
 let main argv =
     let web3 = Web3(Account(hardhatPrivKey), hardhatURI)
@@ -35,10 +44,8 @@ let main argv =
     // print block number before hardhat_reset
     sprintf "Before: %A" |> printBlockNumber web3
 
-    // log what the serialization result is
-    HardhatReset() |> JsonConvert.SerializeObject |> printfn "Sending JSON: %s"
-    // send hardhat_reset request with a newly created HardhatReset object. [||] is an array. 
-    web3.Client.SendRequestAsync(new RpcRequest(2, "hardhat_reset", [|HardhatReset()|])) |> runNowWithoutResult
+    let input = HardhatResetInput(Forking=HardhatForkInput(BlockNumber=12330245UL,JsonRpcUrl=alchemyKey))
+    HardhatReset(web3.Client).SendRequestAsync input null |> runNow |> printfn "result: %b"
     
     // print block number after hardhat_reset
     sprintf "After: %A" |> printBlockNumber web3
